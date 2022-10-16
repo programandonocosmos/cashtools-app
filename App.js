f4ae769054b3533518109edcd5cbe70b95e94bb0
Davi Suga <daviciencia1@gmail.com>
2022-10-16 19:49:58 -0300

    feat: theme config

--- App.js
+++ App.js
@@ -1,14 +1,6 @@
 import { extendTheme, NativeBaseProvider } from "native-base";
 import Routes from "./src/App.bs";
 const App = () => {
-  const theme = extendTheme({
-    useSystemColorMode: false,
-    initialColorMode: "dark",
-  });
-  return (
-    <NativeBaseProvider theme={theme}>
-      <Routes />
-    </NativeBaseProvider>
-  );
+  return <Routes />;
 };
 export default App;
