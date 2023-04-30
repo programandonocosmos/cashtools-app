6ea943bbe7021e31c59c4e87cd4fbcbcfc15ab0d
davisuga <daviciencia1@gmail.com>
2023-04-30 18:49:19 -0300

    feat: added .env support

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import { extendTheme, NativeBaseProvider } from "native-base";
+import { env } from "./src/Env.bs";
 import Routes from "./src/App.bs";
+// import StorybookUIRoot from "./.storybook/Storybook";
 const App = () => {
+  // return env.storybookMode ? <StorybookUIRoot /> : <Routes />;
   return <Routes />;
 };
 export default App;
